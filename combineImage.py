659ed3134c977026a1a72577042cefd4515f0bf9
Fangwei <fangweilin00@gmail.com>
2022-07-18 13:53:03 -0700

    combine images

--- combineImage.py
+++ combineImage.py
@@ -1,31 +1,24 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from PositionImageProcessing import PositionImageProcessing
+import matplotlib.image as mpimg
+htich_tuple = ()
+for r in range(1,9):
+    images = ()
+    for i in range(1,13):
+        image = mpimg.imread('processed_position_images/' + 'combined_image_' + str(12 * (r - 1) + i) +'.jpeg')
+        image = cv2.resize(image, None, fx=0.1, fy=0.1)
+        images = images + (image,)
+    htich = np.hstack(images)
+    htich_tuple = htich_tuple + (htich,)
+vtich = np.vstack(htich_tuple)
 
-img1 = cv2.imread("C:/Users/Vibrant/Desktop/openCV/positions/A1 Position1_ch00.tif")
-img2 = cv2.imread("C:/Users/Vibrant/Desktop/openCV/positions/A2 Position1_ch00.tif")
-img3 = cv2.imread("C:/Users/Vibrant/Desktop/openCV/positions/A3 Position1_ch00.tif")
-img4 = cv2.imread("C:/Users/Vibrant/Desktop/openCV/positions/A4 Position1_ch00.tif")
+# img1 = cv2.resize(img1, None, fx=0.02, fy=0.02)    #为了完整显示，缩小一倍
 
-img1 = cv2.resize(img1, None, fx=0.05, fy=0.05)    #为了完整显示，缩小一倍
-# img2 = cv2.resize(img2, None, fx=0.05, fy=0.05)    #为了完整显示，缩小一倍
-# img3 = cv2.resize(img3, None, fx=0.05, fy=0.05)    #为了完整显示，缩小一倍
-# img4 = cv2.resize(img4, None, fx=0.05, fy=0.05)    #为了完整显示，缩小一倍
+cv2.putText(vtich, 'A1', (int(500), int(500)), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 0), 3)
 
-
-
-# # blur2 = cv2.blur(img, (2,2))
-# # blur3 = cv2.blur(img, (5,5))
-# # blur4 = cv2.blur(img, (10,10))
-
-# htich = np.hstack((img1,img2))
-# htich2 = np.hstack((img3,img4))
-# vtich = np.vstack((htich, htich2))
-
-# cv2.imshow("merged_img", img1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 fig,ax = plt.subplots(1)
-ax.imshow(img1)
+ax.imshow(vtich)
 plt.show()
